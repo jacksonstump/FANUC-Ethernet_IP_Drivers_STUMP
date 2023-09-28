ea06556cc8b6b3833f40c84c2d9a36b2f80e8b2b
jacksonstump <145722421+jacksonstump@users.noreply.github.com>
2023-09-27 19:43:36 -0700

    FINALLL

--- Lab3_STUMP.py
+++ Lab3_STUMP.py
@@ -7,10 +7,10 @@ INT_Dice1   = [825.267,     351.151,    -180,       -179.286,   0.64,       31.0
 Dice_Grab1  = [825.267,     351.151,    -202.474,   -179.286,   0.64,       31.021]
 INT_DICE2   = [803.57,      670,        -50,        -180,       0.64,       31.021]
 DICE_DROP1  = [803.57,      670,        -202,       -180,       0.64,       31.021]
-INT_DICE3   = [112.372,     684.5,      -50,        -180,       0.64,       31.021]
-Dice_Grab2  = [112.372,     684.5,      -202,       -180,       0.64,       31.021]
-DICE_DROP2  = [886,         315,        -202.5,     180,        0.696,      -59]
-INT_DICE4   = [886,         315,        -180,       180,        0.696,      -59]
+INT_DICE3   = [112.372,     675,        -50,        -180,       0.64,       31.021]
+Dice_Grab2  = [112.372,     675,        -202,       -180,       0.64,       31.021]
+DICE_DROP2  = [886,         320,        -202.5,     180,        0.696,      -59]
+INT_DICE4   = [886,         320,        -180,       180,        0.696,      -59]
 Rotate      = [0, 0, 0, 0, 90]
 
 def move_robot_linear(beaker, cartcoords):
@@ -38,7 +38,7 @@ Robot_IP = '172.29.209.124'
 def main():
     
     beaker = robot(Robot_IP)
-    beaker.set_speed(200)
+    beaker.set_speed(300)
     cycles = 1
     beaker.conveyor("stop")
     Conveyor_Forward = False
@@ -63,56 +63,48 @@ def main():
         if R_1 == "1":
             print("Dice identified...Grabbing Dice")
             move_robot_linear(beaker, Dice_Grab1)
-            gripper(beaker,"close",0.1)
+            gripper(beaker,"close",0.2)
             
             #print("Clearance Move")
             move_robot_linear(beaker, INT_DICE2)
             
             #print("Set Dice on Conveyor")
             move_robot_linear(beaker, DICE_DROP1)
-            gripper(beaker,"open",0.1)
+            gripper(beaker,"open",0.2)
             
             #print("Clearance Move")
             move_robot_linear(beaker, INT_DICE2)
 
-
-               #if left_prox:
-               # print("Dice identified at right sensor")
-               # beaker.conveyor("forward")
-                
-               # print("Postion above pickup")
-               # move_robot_linear(beaker, INT_DICE3)
+            #left_prox = beaker.conveyor_proximity_sensor("left")
+            #if left_prox:
+            #    print("Dice identified at left sensor")
+            #    beaker.conveyor("forward")
                
-               # while not beaker.conveyor_proximity_sensor("right"):
-               #     continue
-               # print("Right sensor active, stopping belt")
-               # beaker.conveyor("stop")
-               # print("belt stopped")
+            #    move_robot_linear(beaker, INT_DICE3)
+               
+            #    while not beaker.conveyor_proximity_sensor("right"):
+            #        continue
+            #    beaker.conveyor("stop")
+            #    print("belt stopped")
             
             left_prox = beaker.conveyor_proximity_sensor("left")
             if left_prox:
                 beaker.conveyor("forward")
                 move_robot_linear(beaker, INT_DICE3)
-                print("Dice identified at right sensor")
+                print("Dice identified at Left sensor")
                 Conveyor_Forward = True
-                #print("Postion above pickup")
-                while Conveyor_Forward:
-                    right_prox = beaker.conveyor_proximity_sensor("right")
-                    if right_prox:
-                        beaker.conveyor("stop")
-                        print("Dice identified at left sensor")
-                        Conveyor_Forward = False
-                    else:
-                        print("Proximity Sensor Not Triggered")
-                
             else:
                 print("Proximity Sensor Not Triggered")
-
-            
-            
+                
+            while Conveyor_Forward:
+                if beaker.conveyor_proximity_sensor("right"):
+                    beaker.conveyor("stop")
+                    print("Dice identified at right sensor")
+                    Conveyor_Forward = False
+           
             #print("Grabbing Dice...")        
             move_robot_linear(beaker, Dice_Grab2)
-            gripper(beaker,"close",0.1)
+            gripper(beaker,"close",0.2)
             
             print("Clearance Move")
             move_robot_linear(beaker, INT_DICE3)
@@ -124,7 +116,7 @@ def main():
             
             print("Dropping dice...")
             move_robot_linear(beaker, DICE_DROP2)
-            gripper(beaker,"open",0.1)
+            gripper(beaker,"open",0.2)
 
             move_robot_linear(beaker, INT_DICE4)
             
